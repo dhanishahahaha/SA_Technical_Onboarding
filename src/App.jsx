2ae2b7c107b839e677999f38c207024f5216036b
Dhanisha  Sharma <120709246+dhanishahahaha@users.noreply.github.com>
2023-08-04 17:56:58 +0000

    Please enter the commit message for your changes.

--- src/App.jsx
+++ src/App.jsx
@@ -24,9 +24,9 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
+  name: "Dhanisha Sharma",
+  title: "Microsoft Learn Student Ambassador",
+  email: "Dhanisha.Sharma@studentambassadors.com",
   gitHub: "microsoft",
   instagram: "microsoft",
   linkedIn: "satyanadella",
